71eea76f070cd245afbd241aeffb37ed4999f39f
mariohaya <152241805+mariohaya@users.noreply.github.com>
2026-03-21 10:55:35 -0500

    added logret cumret

--- ticker2csv.py
+++ ticker2csv.py
@@ -11,8 +11,9 @@ Created on Mon Mar  9 13:21:02 2026
 ##### libs
 import yfinance as yf
 import pandas as pd
-from pathlib import Path
+import numpy as np
 
+from pathlib import Path
 from datetime import datetime
 
 ##### Initial input
@@ -38,14 +39,29 @@ df = yf.download(
     auto_adjust=True,
     progress=True)
 
+## daily ret
+dret = df["Close"].pct_change()
+dret = dret.rename(columns=lambda x: f"Daily Ret_{x}")
+dftotal = pd.concat([df, dret], axis=1)
+
+## log ret cum ret
+logclose = df["Close"]
+logret = np.log(logclose / logclose.shift(1))
+logret = logret.rename(columns=lambda x: f"Log Ret_{x}")
+cumret = (1 + dret).cumprod() - 1
+cumret = cumret.rename(columns=lambda x: f"Cum Ret_{x}")
+dftotal = pd.concat([dftotal, cumret], axis=1)
+dftotal = pd.concat([dftotal, logret], axis=1)
+
+
+
+
 #### output
 timestamp = datetime.now().strftime("%b%d-%H%M").lower()
 filename = "_".join(tickerlist) + "_" + "" + timestamp + ".csv" 
-df.to_csv(filename)
+dftotal.to_csv(filename)
 
 filepath = Path(filename).resolve()
 print(f"Successfully exported to {filepath.resolve()}")
-print(df.head())
-
-
+print(dftotal.head())
 
